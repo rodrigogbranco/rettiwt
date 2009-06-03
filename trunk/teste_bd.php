1f2727ec5cd41ea6ef49599683ff98862e14eac0
renanmarks <renanmarks@b9d7bfd4-23d6-11de-a78b-a7aca27b2395>
2009-06-03 18:56:20 +0000

    Mudado cabecalho de latin1 pra utf8

--- trunk/teste_bd.php
+++ trunk/teste_bd.php
@@ -6,21 +6,14 @@ global $bd;
 
 $teste = new Teste();
 
-$teste->nome = "Teste2";
-$teste->idade = "10";
-
-var_dump($teste);
+$teste->nome = "Mais um teste";
+$teste->idade = "20";
 
 $teste->save();
 
 //$teste->remove();
-
-var_dump($teste);
-
 //$teste->save();
-
 //var_dump($teste);
-
 //$resultado = $bd->fetch_row('SELECT * FROM teste');
 //var_dump($resultado);
 
