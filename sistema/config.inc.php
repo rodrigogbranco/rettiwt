e1b4ee52b875524fccbd1966615f00633139b3a6
karennakazato <karennakazato@b9d7bfd4-23d6-11de-a78b-a7aca27b2395>
2009-06-03 19:34:51 +0000

    adicionei o modelo Twitt

--- sistema/config.inc.php
+++ sistema/config.inc.php
@@ -11,8 +11,8 @@ $config = array();
  * Dados do banco de dados.
  */
 $config['bd']['type'] = "mysql";
-$config['bd']['user'] = "root";
-$config['bd']['password'] = "";
+$config['bd']['user'] = "rettiwt";
+$config['bd']['password'] = "rettiwt.09";
 $config['bd']['host'] = "localhost";
 $config['bd']['database'] = "rettiwt";
 
