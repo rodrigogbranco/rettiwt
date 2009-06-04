6c55f9e28cd1f45ea83eda6b0ca6d6dc9a79d859
thalesfduarte <thalesfduarte@b9d7bfd4-23d6-11de-a78b-a7aca27b2395>
2009-06-04 16:16:59 +0000

    tem funçao de inserir twitts e de mostrar os twitts

--- sistema/controller/TwittControler.php
+++ sistema/controller/TwittControler.php
@@ -10,7 +10,7 @@ require_once("sistema/model/Twitt.php");
 /*Classe do Controlador*/
 class TwittController
 {
-	//Usuário ativo para controle de seção
+	//ID do Usuário ativo para o controle dos twitts
 	var $twittUser = null;
 	
 	/*Construtor*/
@@ -24,21 +24,62 @@ class TwittController
 	function showMessage()
 	{
 		
-		$select =  "SELECT id, id_user, text, id_multimida, reply , DATE_FORMAT(date,'%d/%m/%Y') as date, TIME_FORMAT(time,'%H:%i:%S') as time FROM twitt where id_user = ".$this->twittUser." ORDER BY date DESC, time DESC" ;
+		$select =  "SELECT id, id_user, text, id_multimidia, reply , DATE_FORMAT(date,'%d/%m/%Y') as date, TIME_FORMAT(time,'%H:%i:%S') as time FROM twitt where id_user = ".$this->twittUser." ORDER BY date DESC, time DESC" ;
 				    
 		global $bd;
 		
 		$twitts = $bd->fetch_array_list($select,'Twitt');
-/*		
-		foreach($twitts as $twitt)
-		{
-			$twitt->texto;
-		}
-		*/
+		
+		//var_dump($twitts);
+		return $twitts;
+	}
+
+	//retorna um array com todos objetos  todos twitts enviados pelo usuário, 
+	//todos os twits enviados pelas pessoas que ele segue e todos o twitts de resposta aos twitts que ele enviou
+	function showAllMessage()
+	{
+		
+		$select =  "SELECT t.id, t.id_user, t.text, t.id_multimidia, t.reply , DATE_FORMAT(t.date,'%d/%m/%Y') as date, TIME_FORMAT(t.time,'%H:%i:%S') as time FROM twitt t where t.id_user = ".$this->twittUser."   	UNION 
+		 SELECT t2.id, t2.id_user, t2.text, t2.id_multimidia, t2.reply , DATE_FORMAT(t2.date,'%d/%m/%Y') as date, TIME_FORMAT(t2.time,'%H:%i:%S') as time FROM twitt t2 where t2.reply IN (SELECT t3.id FROM twitt t3 where t3.id_user =  ".$this->twittUser.")  	UNION
+		 SELECT t4.id, t4.id_user, t4.text, t4.id_multimidia, t4.reply , DATE_FORMAT(t4.date,'%d/%m/%Y') as date, TIME_FORMAT(t4.time,'%H:%i:%S') as time FROM twitt t4 where t4.id_user IN (SELECT f.id_user_follow FROM follow f where f.id_user =  ".$this->twittUser.")  UNION
+		 SELECT t5.id, t5.id_user, t5.text, t5.id_multimidia, t5.reply , DATE_FORMAT(t5.date,'%d/%m/%Y') as date, TIME_FORMAT(t5.time,'%H:%i:%S') as time FROM twitt t5 where t5.reply IN (SELECT t6.id FROM twitt t6 where t6.id_user IN (SELECT f2.id_user_follow FROM follow f2 where f2.id_user =  ".$this->twittUser.")) ORDER BY date DESC, time DESC " ;
+				    
+		global $bd;
+		
+		$twitts = $bd->fetch_array_list($select,'Twitt');
+
 		
 		var_dump($twitts);
 		return $twitts;
 	}
-
+	
+	//recebe os paramentros de um twitt e cria um novo twitt e insere no banco de dados
+	function insertTwitt( $text, $id_multimidia = null, $reply = null){
+	
+		/*$twitt = new Twitt();
+		$twitt->id_user = $this->twittUser;
+		$twitt->text = $text;
+		$twitt->date = $date;
+		$twitt->time = $time;
+		$twitt->id_multimidia = $id_multimidia;
+		$twitt->reply = $reply;*/
+		
+	$insert = "INSERT INTO twitt (id_user, text, date, time, id_multimidia, reply) 
+				 VALUES ('.$this->twittUser.', '.$text', curdate(), curtime() ,'.$id_multimidia.', '.$reply.') "; 
+				 
+	/*	$insert = "INSERT INTO twitt (id_user, text, date, time, id_multimidia, reply) 
+				 VALUES ( 6 , 'sdftdfg', curdate(), curtime() ,2, 8) "; */
+			 
+		
+		global $bd;
+		
+		 $bd->query($insert);
+		//$twitt->validTwitt();
+		//$twitt->save();
+	
+	}
+	
+	
+	
 }
 ?>
