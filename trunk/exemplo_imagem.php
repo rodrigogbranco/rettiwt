243f666187dd99aec8dedaa9db4b813e8ab236f8
renanmarks <renanmarks@b9d7bfd4-23d6-11de-a78b-a7aca27b2395>
2009-06-04 23:03:24 +0000

    Avatar funcionando

--- trunk/exemplo_imagem.php
+++ trunk/exemplo_imagem.php
@@ -7,13 +7,15 @@ require_once("sistema/sistema.php");
 require_once("sistema/controller/Controller.php");
 require_once("sistema/componentes/multimidia/multimidia.class.php");
 
-$usuario = new User('alias', 'renan');
+$usuario = new User('alias', 'karenLinda');
 
 $imagem = new Multimidia();
 $imagem->tipo = Multimidia::IMAGEM;
 $imagem->id_usuario = $usuario->id;
 $imagem->save('arquivo');
-$imagem->resizeImage(60,60);
+$usuario->foto_id_multimidia = $imagem->id;
+$usuario->save();
+//$imagem->resizeImage(60,60);
 
 
 ?>
